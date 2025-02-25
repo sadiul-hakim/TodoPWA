2eab39a097df020cdffec037e2e10e9d62643adb
Sadiul Hakim <sadiulhakim@gmail.com>
2025-02-25 10:34:07 +0600

    Cache

--- src/main/resources/static/sw.js
+++ src/main/resources/static/sw.js
@@ -1,13 +1,41 @@
-self.addEventListener("install", (event) => {
-    console.log("Service Worker Installed");
-    event.waitUntil(self.skipWaiting());
+const CACHE_NAME = 'todo-list-cache-v1';
+const urlsToCache = [
+    '/icon-192x192.png',
+    '/icon-512x512.png',
+    '/screenshot1.png',
+    '/manifest.json'
+];
+
+// Install service worker and cache static assets
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(urlsToCache))
+    );
 });
 
-self.addEventListener("activate", (event) => {
-    console.log("Service Worker Activated");
-    event.waitUntil(self.clients.claim());
+// Serve assets from cache when offline
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((cachedResponse) => {
+                return cachedResponse || fetch(event.request);
+            })
+    );
 });
 
-self.addEventListener("fetch", (event) => {
-    event.respondWith(fetch(event.request));
+// Clear old caches on service worker activation
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
 });
